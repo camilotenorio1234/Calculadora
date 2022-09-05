05dd1bab9495bd3c4518b16e5d878e75bc7cb3a4
camilotenorio1234 <99371279+camilotenorio1234@users.noreply.github.com>
2022-09-05 18:19:16 -0500

    Calculadora
    Este codigo implementa una calculadora en android estudio

---
+++ MyCalcarculator/app/src/main/java/com/example/mycalcarculator/Operaciones.java
@@ -0,0 +1,14 @@
+package com.example.mycalcarculator;
+
+import android.widget.EditText;
+
+public interface Operaciones {
+
+    int Sumar(EditText A, EditText B);
+    int Resta(EditText A, EditText B);
+    int Multiplicacion(EditText A, EditText B);
+    float Divicion(EditText A, EditText B);
+
+
+
+}
---
+++ MyCalcarculator/app/src/main/java/com/example/mycalcarculator/operaciones_metodos.java
@@ -0,0 +1,32 @@
+package com.example.mycalcarculator;
+
+import android.widget.EditText;
+
+
+public class operaciones_metodos implements Operaciones{
+
+    public int Sumar(EditText A, EditText B) {
+        int operacion;
+        return operacion=Integer.parseInt(A.getText().toString())+Integer.parseInt(B.getText().toString());
+    }
+
+    @Override
+    public int Resta(EditText A, EditText B) {
+        int operacion;
+        return operacion=Integer.parseInt(A.getText().toString())-Integer.parseInt(B.getText().toString());
+    }
+
+    @Override
+    public int Multiplicacion(EditText A, EditText B) {
+        int operacion;
+        return operacion=Integer.parseInt(A.getText().toString())*Integer.parseInt(B.getText().toString());
+    }
+
+    @Override
+    public float Divicion(EditText A, EditText B) {
+        int operacion;
+        return operacion=Integer.parseInt(A.getText().toString())/Integer.parseInt(B.getText().toString());
+    }
+
+
+}
